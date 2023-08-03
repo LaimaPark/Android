202c224b1b444610f16ccb5d8603efeae4ea0100
Jaewon Park <laimaurora@gmail.com>
2023-08-03 21:49:50 +0900

    Compose Codelabs] BasicsCodelab - 호이스팅 전까지

--- Compose-practice/BasicsCodelab/app/src/main/java/com/laima/basicscodelab/MainActivity.kt
+++ Compose-practice/BasicsCodelab/app/src/main/java/com/laima/basicscodelab/MainActivity.kt
@@ -3,12 +3,14 @@ package com.laima.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,28 +28,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(modifier: Modifier = Modifier) {
-    /* modifier 를 받아서 ui 변경할 수 있다. */
-    Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Greeting("Android")
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose"),
+) {
+    Column(modifier = modifier) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Surface( /* 배경색 설정 */ color = MaterialTheme.colorScheme.primary) {
-        Text(
-            text = "Hello $name!",
-            /* modifier = 형태 바꾸기 */
-            modifier = Modifier.padding(24.dp)
-        )
+    // prevent from recomposition!
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    Surface(
+        /* 배경색 설정 */
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        /* 선언형 이어서 할 수 있다 */
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(extraPadding)
+            ) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            ElevatedButton(
+                onClick = { expanded.value = !expanded.value }
+            ) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
