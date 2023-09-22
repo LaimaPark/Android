520a2320e7ca1beb02203b9be15c2ff5871b8704
Jaewon Park <laimaurora@gmail.com>
2023-09-22 15:39:47 +0900

    Compose Lecture - Surface

---
+++ ComposeChapter3/app/src/main/java/compose/laima/composechapter3/SurfaceActivity.kt
@@ -0,0 +1,43 @@
+package compose.laima.chapter_3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import compose.laima.chapter_3.ui.theme.ComposeChapter3Theme
+
+class SurfaceActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeChapter3Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    SurfaceExample("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SurfaceExample(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SurfacePreview() {
+    ComposeChapter3Theme {
+        SurfaceExample("Android")
+    }
+}
