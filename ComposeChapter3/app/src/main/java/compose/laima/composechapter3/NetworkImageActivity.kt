535b95d8712f873695cbbcea847ef63c2e5a979e
Jaewon Park <laimaurora@gmail.com>
2023-09-25 19:53:18 +0900

    Compose Lecture - Network Image

--- ComposeChapter3/app/src/main/java/compose/laima/composechapter3/NetworkImageActivity.kt
+++ ComposeChapter3/app/src/main/java/compose/laima/composechapter3/NetworkImageActivity.kt
@@ -1,8 +1,9 @@
-package compose.laima.composechapter3
+package compose.laima.chapter_3
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,34 +11,37 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import compose.laima.composechapter3.ui.theme.ComposeChapter3Theme
+import coil.compose.AsyncImage
+import coil.compose.rememberImagePainter
+import compose.laima.chapter_3.ui.theme.ComposeChapter3Theme
 
-class WebImageActivity : ComponentActivity() {
+class NetworkImageActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeChapter3Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    WebImageExample("Android")
-                }
+                CoilExample()
             }
         }
     }
 }
 
 @Composable
-fun WebImageExample(name: String) {
-    Text(text = "Hello $name!")
+fun CoilExample() {
+    // rememberImagePainter Ver
+    val painter = rememberImagePainter(data = "URL")
+    Image(painter = painter, contentDescription = "엔텔로프 캐넌")
+
+    // AsyncImage Ver
+    AsyncImage(model = "URL", contentDescription = "엔텔로프 캐넌")
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun WebImagePreview() {
+fun CoilPreview() {
     ComposeChapter3Theme {
-        WebImageExample("Android")
+        CoilExample()
     }
 }
