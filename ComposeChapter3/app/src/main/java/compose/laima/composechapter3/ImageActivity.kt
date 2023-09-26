261f2101e10688dc6299a160769cf854dcb60ced
Jaewon Park <laimaurora@gmail.com>
2023-09-26 23:34:48 +0900

    Compose Lecture - Card View

--- ComposeChapter3/app/src/main/java/compose/laima/composechapter3/ImageActivity.kt
+++ ComposeChapter3/app/src/main/java/compose/laima/composechapter3/ImageActivity.kt
@@ -5,17 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import compose.laima.chapter_3.ui.theme.ComposeChapter3Theme
+import compose.laima.composechapter3.ui.theme.ComposeChapter3Theme
 
 class ImageActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,8 +36,6 @@ fun ImageExample() {
             imageVector = Icons.Filled.Settings,
             contentDescription = "세팅"
         )
-
-
 
 
     }
