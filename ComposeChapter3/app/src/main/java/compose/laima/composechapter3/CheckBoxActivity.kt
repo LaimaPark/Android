f5188bb493761fbff34512201613ada6caadb2fb
Jaewon Park <laimaurora@gmail.com>
2023-09-30 16:13:10 +0900

    Compose Lecture - CheckBox Destruction

--- ComposeChapter3/app/src/main/java/compose/laima/composechapter3/CheckBoxActivity.kt
+++ ComposeChapter3/app/src/main/java/compose/laima/composechapter3/CheckBoxActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import android.widget.CheckBox
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Checkbox
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,15 +31,52 @@ fun CheckBoxExample() {
     Row(verticalAlignment = Alignment.CenterVertically) {
         // 수직정렬을 위해 verticalAlignment
 
-        var checked = false
+//        var checked = remember { mutableStateOf(false) }
+//        Checkbox(
+//            checked = checked.value,
+//            onCheckedChange = {
+//                checked.value = !checked.value
+//                // 값이 바뀌어야 recomposition
+//            }
+//        )
+
+        // delegated properties (by)
+        // checked 가 property 인것 처럼
+        // .value 인것 처럼 하는 것
+        var checked by remember { mutableStateOf(false) }
         Checkbox(
             checked = checked,
             onCheckedChange = {
                 checked = !checked
+                // 값이 바뀌어야 recomposition
             }
         )
 
-        Text(text = "프로그래머입니까?")
+
+        // destruction
+        // var (a, b) =  listOf(1, 2)
+//        var (checked2, setChecked) = remember { mutableStateOf(false) }
+//        Checkbox(
+//            checked = checked2,
+////            onCheckedChange = {
+////                // it 이 반전된 값을 줌
+////                setChecked(it)
+////                // 값이 바뀌어야 recomposition
+////            }
+//            onCheckedChange = setChecked // 이렇게 가능
+//        )
+        val (getter, setter) = remember { mutableStateOf(false) }
+        Checkbox(
+            checked = getter,
+            onCheckedChange = setter // 이렇게 가능
+        )
+
+        Text(
+            text = "프로그래머입니까?",
+            modifier = Modifier.clickable {
+                setter(!getter)
+            }
+        )
         
     }
 }
